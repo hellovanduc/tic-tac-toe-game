6ee0e2b9cecf027f018cb747ed9253e0945bc71a
Nguyen Van Duc <nguyenduc21022k@gmail.com>
2021-09-26 09:30:40 +0700

    Calculate and display winner

--- src/components/Board.js
+++ src/components/Board.js
@@ -5,19 +5,55 @@ const Board = () => {
   const [state, setSate] = useState({
     squareValues: Array(9).fill(null),
     nextPlayer: "X",
+    winner: null,
   });
 
   const squareClickHandler = (i) => {
     setSate((prevState) => {
-      if (prevState.squareValues[i] === null) {
+      if (prevState.squareValues[i] !== null || prevState.winner !== null) {
+        return prevState;
+      } else {
         let newSquareValues = prevState.squareValues.slice();
         newSquareValues[i] = prevState.nextPlayer;
 
-        let newNextPlayer  = prevState.nextPlayer === "X" ? "O" : "X"
+        let newNextPlayer = prevState.nextPlayer === "X" ? "O" : "X";
 
-        return {...prevState, nextPlayer: newNextPlayer, squareValues: newSquareValues};
+        let newWinner = calculateWinner(newSquareValues);
+
+        return {
+          ...prevState,
+          nextPlayer: newNextPlayer,
+          squareValues: newSquareValues,
+          winner: newWinner,
+        };
       }
     });
+  };
+
+  const calculateWinner = (squares) => {
+    const lines = [
+      [0, 1, 2],
+      [3, 4, 5],
+      [6, 7, 8],
+      [0, 3, 6],
+      [1, 4, 7],
+      [2, 5, 8],
+      [0, 4, 8],
+      [2, 4, 6],
+    ];
+
+    for (let i = 0; i < lines.length; i++) {
+      const [a, b, c] = lines[i];
+      if (
+        squares[a] &&
+        squares[a] === squares[b] &&
+        squares[a] === squares[c]
+      ) {
+        return squares[a];
+      }
+    }
+
+    return null;
   };
 
   const renderSquare = (i) => {
@@ -32,6 +68,9 @@ const Board = () => {
 
   return (
     <div>
+      {state.winner !== null && (
+        <div className="status">Winner is {state.winner}</div>
+      )}
       <div className="status">Next player: {state.nextPlayer}</div>
       <div className="board-row">
         {renderSquare(0)}

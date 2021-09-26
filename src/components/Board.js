42db2ea8e7553dc6ded02fe346358c775ee1bdbd
Nguyen Van Duc <nguyenduc21022k@gmail.com>
2021-09-26 10:23:27 +0700

    Lift state up to GameComponent

--- src/components/Board.js
+++ src/components/Board.js
@@ -1,65 +1,18 @@
-import { useState } from "react";
 import Square from "./Square";
 
-const Board = () => {
-  const [state, setSate] = useState({
-    squareValues: Array(9).fill(null),
-    nextPlayer: "X",
-    winner: null,
-  });
-
+const Board = (props) => {
   const squareClickHandler = (i) => {
-    setSate((prevState) => {
-      if (prevState.squareValues[i] !== null || prevState.winner !== null) {
-        return prevState;
-      } else {
-        let newSquareValues = prevState.squareValues.slice();
-        newSquareValues[i] = prevState.nextPlayer;
+    if (!props.isGameOver && props.squares[i] === null) {
+      props.squares[i] = props.nextPlayer;
 
-        let newNextPlayer = prevState.nextPlayer === "X" ? "O" : "X";
-
-        let newWinner = calculateWinner(newSquareValues);
-
-        return {
-          ...prevState,
-          nextPlayer: newNextPlayer,
-          squareValues: newSquareValues,
-          winner: newWinner,
-        };
-      }
-    });
-  };
-
-  const calculateWinner = (squares) => {
-    const lines = [
-      [0, 1, 2],
-      [3, 4, 5],
-      [6, 7, 8],
-      [0, 3, 6],
-      [1, 4, 7],
-      [2, 5, 8],
-      [0, 4, 8],
-      [2, 4, 6],
-    ];
-
-    for (let i = 0; i < lines.length; i++) {
-      const [a, b, c] = lines[i];
-      if (
-        squares[a] &&
-        squares[a] === squares[b] &&
-        squares[a] === squares[c]
-      ) {
-        return squares[a];
-      }
+      props.onSquaresChange(props.squares);
     }
-
-    return null;
   };
 
   const renderSquare = (i) => {
     return (
       <Square
-        value={state.squareValues[i]}
+        value={props.squares[i]}
         index={i}
         onSquareClick={squareClickHandler}
       />
@@ -68,10 +21,6 @@ const Board = () => {
 
   return (
     <div>
-      {state.winner !== null && (
-        <div className="status">Winner is {state.winner}</div>
-      )}
-      <div className="status">Next player: {state.nextPlayer}</div>
       <div className="board-row">
         {renderSquare(0)}
         {renderSquare(1)}
--- src/components/Game.js
+++ src/components/Game.js
@@ -1,13 +1,73 @@
+import { useState } from "react";
 import Board from "./Board";
 
 const Game = () => {
+  const [state, setSate] = useState({
+    history: [{ squares: Array(9).fill(null) }],
+    nextPlayer: "X",
+    winner: null,
+  });
+
+  const calculateWinner = (squares) => {
+    const lines = [
+      [0, 1, 2],
+      [3, 4, 5],
+      [6, 7, 8],
+      [0, 3, 6],
+      [1, 4, 7],
+      [2, 5, 8],
+      [0, 4, 8],
+      [2, 4, 6],
+    ];
+
+    for (let i = 0; i < lines.length; i++) {
+      const [a, b, c] = lines[i];
+      if (
+        squares[a] &&
+        squares[a] === squares[b] &&
+        squares[a] === squares[c]
+      ) {
+        return squares[a];
+      }
+    }
+
+    return null;
+  };
+
+  const squaresChangeHandler = (newSquares) => {
+    setSate((prevState) => {
+      let newHistory = prevState.history.slice();
+      newHistory.push({ squares: newSquares });
+
+      let newNextPlayer = prevState.nextPlayer === "X" ? "O" : "X";
+
+      let newWinner = calculateWinner(newSquares);
+
+      return {
+        ...prevState,
+        history: newHistory,
+        nextPlayer: newNextPlayer,
+        winner: newWinner,
+      };
+    });
+  };
+  const status =
+    state.winner !== null
+      ? `Winner: ${state.winner}`
+      : `Next player: ${state.nextPlayer}`;
+
   return (
     <div className="game">
       <div className="game-board">
-        <Board />
+        <Board
+          squares={state.history.at(-1).squares}
+          nextPlayer={state.nextPlayer}
+          isGameOver={state.winner !== null}
+          onSquaresChange={squaresChangeHandler}
+        />
       </div>
       <div className="game-info">
-        <div>{/* status */}</div>
+        <div>{status}</div>
         <ol>{/* TODO */}</ol>
       </div>
     </div>

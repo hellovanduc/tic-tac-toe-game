8dbd47ae211f08d0b934dfb25bdda92b8a8c780c
Nguyen Van Duc <nguyenduc21022k@gmail.com>
2021-09-26 08:24:38 +0700

    Make Board control value of squares and create nextPlayer state

--- src/components/Board.js
+++ src/components/Board.js
@@ -2,12 +2,43 @@ import React from "react";
 import Square from "./Square";
 
 class Board extends React.Component {
+  constructor(props) {
+    super(props);
+    this.state = {
+      squareValues: Array(9).fill(null),
+      nextPlayer: "X",
+    };
+  }
+
+  squareClickHandler(i) {
+    this.setState((prevState) => {
+      if (prevState.squareValues[i] === null) {
+        let newSquareValues = prevState.squareValues.slice();
+        newSquareValues[i] = prevState.nextPlayer;
+
+        let newNextPlayer = prevState.nextPlayer === "X" ? "O" : "X";
+
+        return {
+          ...prevState,
+          squareValues: newSquareValues,
+          nextPlayer: newNextPlayer,
+        };
+      }
+    });
+  }
+
   renderSquare(i) {
-    return <Square />;
+    return (
+      <Square
+        value={this.state.squareValues[i]}
+        index={i}
+        onSquareClick={this.squareClickHandler.bind(this)}
+      />
+    );
   }
 
   render() {
-    const status = "Next player: X";
+    const status = `Next player: ${this.state.nextPlayer}`;
 
     return (
       <div>
--- src/components/Square.js
+++ src/components/Square.js
@@ -1,8 +1,16 @@
 import React from "react";
 
 class Square extends React.Component {
+  clickHandler() {
+    this.props.onSquareClick(this.props.index);
+  }
+
   render() {
-    return <button className="square">{/* TODO */}</button>;
+    return (
+      <button className="square" onClick={this.clickHandler.bind(this)}>
+        {this.props.value}
+      </button>
+    );
   }
 }
 

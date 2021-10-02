784f7cb1666d2bec97b086a0be91e32a5f49e0b8
Nguyen Van Duc <nguyenduc21022k@gmail.com>
2021-10-02 18:41:02 +0700

    Reallocate calculateWinner and convertSquareIndexToLocation to GameHelper module

--- src/components/Game.js
+++ src/components/Game.js
@@ -2,6 +2,7 @@ import { useState } from "react";
 import Board from "./Board/Board";
 import GameSetting from "./GameSetting/GameSetting";
 import GameInfor from "./GameInfor/GameInfor";
+import { calculateWinner, convertSquareIndexToLocation } from "./GameHelper";
 
 const DEFAULT_NUM_OF_COLS = 5;
 const DEFAULT_NUM_OF_ROWS = 5;
@@ -27,125 +28,6 @@ const Game = () => {
 
   const current = state.history[state.move];
 
-  const calculateWinner = (squares, currentLocation, currentPlayer) => {
-    const locations = convertSquaresToLocations(squares);
-
-    const isValidRow = (row) => {
-      return row >= 0 && row < numOfRows;
-    };
-
-    const isValidCol = (col) => {
-      return col >= 0 && col < numOfCols;
-    };
-
-    const processToReturn = (hightLightLocations) => {
-      let hightLightSquares = hightLightLocations.map(
-        (location) => location.row * numOfCols + location.col
-      );
-
-      return [currentPlayer, hightLightSquares];
-    };
-
-    let hightLightLocations = [];
-
-    for (
-      let col = currentLocation.col - 4;
-      col <= currentLocation.col + 4;
-      col++
-    ) {
-      if (
-        isValidCol(col) &&
-        locations[currentLocation.row][col] === currentPlayer
-      ) {
-        hightLightLocations.push({ row: currentLocation.row, col: col });
-        if (hightLightLocations.length === 5) {
-          return processToReturn(hightLightLocations);
-        }
-      } else {
-        hightLightLocations = [];
-      }
-    }
-
-    for (
-      let row = currentLocation.row - 4;
-      row <= currentLocation.row + 4;
-      row++
-    ) {
-      if (
-        isValidRow(row) &&
-        locations[row][currentLocation.col] === currentPlayer
-      ) {
-        hightLightLocations.push({ row: row, col: currentLocation.col });
-        if (hightLightLocations.length === 5) {
-          return processToReturn(hightLightLocations);
-        }
-      } else {
-        hightLightLocations = [];
-      }
-    }
-
-    for (let i = -4; i <= 4; i++){
-      let row = currentLocation.row + i
-      let col = currentLocation.col + i
-      if (isValidRow(row) && isValidCol(col) && locations[row][col] === currentPlayer) {
-        hightLightLocations.push({ row, col })
-        if (hightLightLocations.length === 5) {
-          return processToReturn(hightLightLocations);
-        }
-      }
-      else {
-        hightLightLocations = []
-      }
-    }
-
-    for (let i = -4; i <= 4; i++) {
-      let row = currentLocation.row - i;
-      let col = currentLocation.col + i;
-      if (
-        isValidRow(row) &&
-        isValidCol(col) &&
-        locations[row][col] === currentPlayer
-      ) {
-        hightLightLocations.push({ row, col });
-        if (hightLightLocations.length === 5) {
-          return processToReturn(hightLightLocations);
-        }
-      } else {
-        hightLightLocations = [];
-      }
-    }
-
-    return [null, null];
-  };
-
-  const convertSquareIndexToLocation = (squareIndex) => {
-    let col = squareIndex % numOfCols;
-
-    let row = 0;
-    while (squareIndex - numOfCols >= 0) {
-      squareIndex -= numOfCols;
-      row++;
-    }
-
-    return { col, row };
-  };
-
-  const convertSquaresToLocations = (squares) => {
-    let locations = Array(numOfRows);
-
-    for (let i = 0; i < locations.length; i++) {
-      locations[i] = Array(numOfCols);
-    }
-
-    // eslint-disable-next-line
-    squares.map((value, index) => {
-      let location = convertSquareIndexToLocation(index);
-      locations[location.row][location.col] = value;
-    });
-
-    return locations;
-  };
-
   const squaresChangeHandler = (newSquares, squareIndex) => {
     setSate((prevState) => {
       let prevHistory = prevState.history[prevState.move];
@@ -153,12 +35,14 @@ const Game = () => {
 
       let newOutOfEmptySquares = !newSquares.includes(null);
 
-      let newLocation = convertSquareIndexToLocation(squareIndex);
+      let newLocation = convertSquareIndexToLocation(squareIndex, numOfCols);
 
       let [newWinner, newHightLightSquares] = calculateWinner(
         newSquares,
         newLocation,
-        prevHistory.nextPlayer
+        prevHistory.nextPlayer,
+        numOfCols,
+        numOfRows
       );
 
       let newHistory = prevState.history.slice(0, prevState.move + 1).concat([
